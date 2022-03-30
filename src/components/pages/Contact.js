f0d1a354ea09c73ae165e7f2dfb3e42bffe66a49
AndiS90 <87630680+AndiS90@users.noreply.github.com>
2022-03-30 03:27:04 -0500

    updated font once more, removed overly abundant old code in contact

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -44,49 +44,6 @@ function Contact() {
   };
 
 
-
-
-  // // Create state variables for the fields in the form
-  // // We are also setting their initial values to an empty string
-  // const [email, setEmail] = useState('');
-  // const [userName, setUserName] = useState('');
-  // const [message, setMessage] = useState('');
-  // const [errorMessage, setErrorMessage] = useState('');
-
-  // const handleInputChange = (e) => {
-  //   // Getting the value and name of the input which triggered the change
-  //   const { target } = e;
-  //   const inputType = target.name;
-  //   const inputValue = target.value;
-
-  //   // Based on the input type, we set the state of either email, username, and message
-  //   if (inputType === 'email') {
-  //     setEmail(inputValue);
-  //   } else if (inputType === 'userName') {
-  //     setUserName(inputValue);
-  //   } else {
-  //     setMessage(inputValue);
-  //   }
-  // };
-
-  // const handleFormSubmit = (e) => {
-  //   // Preventing the default behavior of the form submit (which is to refresh the page)
-  //   e.preventDefault();
-
-  //   // First we check to see if the email is not valid or if the userName is empty. If so we set an error message to be displayed on the page.
-  //   if (!validateEmail(email) || !userName) {
-  //     setErrorMessage('Email or username is invalid');
-  //     // We want to exit out of this code block if something is wrong so that the user can correct it
-  //     return;      
-  //   }
- 
-
-  //   // If everything goes according to plan, we want to clear out the input after a successful registration.
-  //   setUserName('');
-  //   setMessage('');
-  //   setEmail('');
-  // };
-
   return (
     <div>
       <form className="form">
